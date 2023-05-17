8dc1e31f0535733cbdfadf4969241f0ac174d8e1
dylanmiller5 <132365269+dylanmiller5@users.noreply.github.com>
2023-05-17 14:52:14 +0100

    Fixed a major issue with ID being returned as undefined
    there was a singular speech mark in the wrong place which caused the entire cart system to function incorrectly

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+// error with cartItem being undefiend and i dont know how to fix it
+// Storage.getProduct(id) === undef
+// when ID is being passed into the methods it is being passed as undefiend?
+
 // variables
-console.log("workign")
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const  clearCartBtn= document.querySelector('.clear-btn');
@@ -20,15 +23,24 @@ class Products{
     async getProducts(){
         try {
             let result = await fetch('products.json');
+            console.log(result);
             let data = await result.json();
+            // products is every item in the JSON 
             let products = data.items;
             products = products.map(item => {
                 const { title , price } = item.fields;
-                const { id } = item.sys
+                const { id } = item.sys;
                 const image = item.fields.image.fields.file.url;
+                
                 return {title,price,id,image};
             });
+            
+
+            // the error appears to be here
+            // cannot call specifc values from the array
+            console.log(products.id)
             return products;
+           
         } catch (error) {
             console.log(error);
         }
@@ -37,63 +49,95 @@ class Products{
 
 // display product
 class UI {
+    // passes this method product data
     async displayProducts(products) {
-        console.log(products)
         let result = '';
        // dynamically adds elements to the page from the JSON file
        products.forEach(product => {
             result +=` 
-             <div class="col-sm-6">
-            <div class="card border-warning text-dark">
+            <div id="product">
+                <div class="col-sm-6">
+                    <div class="card border-warning text-dark">
                 
-                <img class="card-img-top" src=${product.image} alt="Item 1 " >
-                <h3 class="card-title">${product.title}</h3>
-                <div class="card-body text-center">
-                <p class="card-text">£${product.price}</p>
-                <button class="bag-btn btn-primary data-id=${product.id}">Add To Basket</button>
-                <br><br>
+                        <img class="card-img-top" src=${product.image} alt="Item 1 " >
+                        <h3 class="card-title">${product.title}</h3>
+                        <div class="card-body text-center">
+                        <p class="card-text">£${product.price}</p>
+                        <button class="bag-btn btn-primary" data-id=${product.id}>Add To Basket</button>
+                        <p>${product.id}</p>
+                        <br><br>
+                    </div>
                 </div>
             </div>
-    </div>
+        </div>
             `;    
         });
-
+        
         productsDOM.innerHTML = result;
     }
+
     getBagButtons(){
         const buttons = [...document.querySelectorAll(".bag-btn")];
-        buttonsDOM = buttons;
+        console.log(buttons)
         buttons.forEach(button => {
+            // THIS ID IS BEING SET TO UNDEFINED
             let id = button.dataset.id;
+            console.log(id);
             let inCart = cart.find(item => item.id === id);
             if (inCart){
                 button.innerText = "In Cart";
                 button.disable = true;
-            }         
+            }    
+
             button.addEventListener("click", (event)=>{
                 event.target.innerText = "In Cart";
                 event.target.disabled = true;
+                
                 // get products from products based on ID
-                let cartItem =Storage.getProducts(id)
+                let cartItem = {...Storage.getProduct(id),
+                amount:1};
                 console.log(cartItem)
-                    // add the product to cart
-                    // save cart in LS
-                    // set values
+
+                 // add the product to cart
+                 cart = [...cart, cartItem];
+
+                // save cart in LS
+                Storage.saveCart(cart)
+                // set values
+                this.setCartValues(cart);
                     // display cart item
                     // show the cart
                 })
         });
+    }
+    setCartValues(cart){
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item =>{
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2))
+        cartItems.innerText = itemsTotal;
+        console.log(cartTotal, cartItems);
     }
 }
 
 // storage class
 class Storage{
     static saveProducts(products){
-        localStorage.setItem("products", JSON.stringify(products))
+        localStorage.setItem("products" , JSON.stringify(products))
     }
-    static getProducts(id){
+    static getProduct(id){
+        // returns my array
         let products = JSON.parse(localStorage.getItem('products'));
+        console.log(products)
+        // products.id and id are undefined
+
         return products.find(product => product.id === id);
+    }
+    static saveCart(cart){
+        localStorage.setItem('cart',JSON.stringify(cart))
     }
 }
 
@@ -109,3 +153,4 @@ document.addEventListener("DOMContentLoaded", () => {
         ui.getBagButtons();
     });
 });
+
